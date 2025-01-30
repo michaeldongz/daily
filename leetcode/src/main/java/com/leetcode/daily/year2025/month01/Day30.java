20af31e877d39c1b3010d4686b80787ce490f3fd
michael <15942842653@163.com>
2025-01-30 09:16:44 +0800

    2025-01-30

---
+++ leetcode/src/main/java/com/leetcode/daily/year2025/month01/Day30.java
@@ -0,0 +1,36 @@
+package com.leetcode.daily.year2025.month01;
+
+import java.util.Arrays;
+
+/**
+ * @author Michael
+ * @date 2025/1/30 09:11
+ * @desc
+ */
+public class Day30 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] intersect(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int length1 = nums1.length, length2 = nums2.length;
+        int[] intersection = new int[Math.min(length1, length2)];
+        int index1 = 0, index2 = 0, index = 0;
+        while (index1 < length1 && index2 < length2) {
+            if (nums1[index1] < nums2[index2]) {
+                index1++;
+            } else if (nums1[index1] > nums2[index2]) {
+                index2++;
+            } else {
+                intersection[index] = nums1[index1];
+                index1++;
+                index2++;
+                index++;
+            }
+        }
+        return Arrays.copyOfRange(intersection, 0, index);
+    }
+}
