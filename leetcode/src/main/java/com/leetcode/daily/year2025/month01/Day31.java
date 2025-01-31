8d2ef06c57bece5c73f771a79636977d4e146e4b
michael <15942842653@163.com>
2025-01-31 09:46:55 +0800

    2025-01-31

---
+++ leetcode/src/main/java/com/leetcode/daily/year2025/month01/Day31.java
@@ -0,0 +1,32 @@
+package com.leetcode.daily.year2025.month01;
+
+/**
+ * @author Michael
+ * @date 2025/1/31 09:45
+ * @desc
+ */
+public class Day31 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public String reverseStr(String s, int k) {
+        int n = s.length();
+        char[] arr = s.toCharArray();
+        for (int i = 0; i < n; i += 2 * k) {
+            reverse(arr, i, Math.min(i + k, n) - 1);
+        }
+        return new String(arr);
+    }
+
+    public void reverse(char[] arr, int left, int right) {
+        while (left < right) {
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
