6b2efd91422f9b77ccd9cf20bc6e0744e00d98bc
michael <15942842653@163.com>
2025-02-01 09:31:01 +0800

    2025-02-1

---
+++ leetcode/src/main/java/com/leetcode/daily/year2025/month02/Day01.java
@@ -0,0 +1,47 @@
+package com.leetcode.daily.year2025.month02;
+
+/**
+ * @author Michael
+ * @date 2025/2/1 09:13
+ * @desc
+ */
+public class Day01 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean search(int[] nums, int target) {
+        int n = nums.length;
+        if (n == 0) {
+            return false;
+        }
+        if (n == 1) {
+            return nums[0] == target;
+        }
+        int l = 0, r = n - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+            if (nums[l] == nums[mid] && nums[mid] == nums[r]) {
+                ++l;
+                --r;
+            } else if (nums[l] <= nums[mid]) {
+                if (nums[l] <= target && target < nums[mid]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[n - 1]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+}
