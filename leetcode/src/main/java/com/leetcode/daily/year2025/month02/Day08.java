454905d925b7e30856c04acfd7308f551f66f213
midong <15942842653@163.com>
2025-02-08 09:46:19 +0800

    2025-02-08

---
+++ leetcode/src/main/java/com/leetcode/daily/year2025/month02/Day08.java
@@ -0,0 +1,34 @@
+package com.leetcode.daily.year2025.month02;
+
+/**
+ * @author: Michael
+ * @date: 2025/2/8
+ * @desc:
+ */
+public class Day08 {
+
+    public static void main(String[] args) {
+        System.out.println(uniquePathsWithObstacles(new int[][]{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}})); // 2
+        System.out.println(uniquePathsWithObstacles(new int[][]{{0, 1}, {0, 0}})); // 1
+    }
+
+    public static int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int n = obstacleGrid.length, m = obstacleGrid[0].length;
+        int[] f = new int[m];
+
+        f[0] = obstacleGrid[0][0] == 0 ? 1 : 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (obstacleGrid[i][j] == 1) {
+                    f[j] = 0;
+                    continue;
+                }
+                if (j - 1 >= 0 && obstacleGrid[i][j - 1] == 0) {
+                    f[j] += f[j - 1];
+                }
+            }
+        }
+
+        return f[m - 1];
+    }
+}
